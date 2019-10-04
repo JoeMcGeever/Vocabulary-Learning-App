c24b623d240202ba1861a35a33189b5054d17db1
Joseph McGeever <mcgeevej@ecm15-mac-12.coventry.ac.uk>
2019-10-04 12:04:41 +0100

    Initial project set up

---
+++ vocabLearner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vocabLearner
+//
+//  Created by Joseph McGeever on 04/10/2019.
+//  Copyright © 2019 Joseph McGeever. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
