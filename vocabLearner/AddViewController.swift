612fe849095fd79125fe94c49de8f473bcd04e65
Joseph McGeever <mcgeevej@coventry.ac.uk>
2019-10-07 10:48:15 +0100

    Linked different view controller files to each tab view ready for coding

---
+++ vocabLearner/AddViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddViewController.swift
+//  vocabLearner
+//
+//  Created by Joseph McGeever on 07/10/2019.
+//  Copyright © 2019 Joseph McGeever. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
