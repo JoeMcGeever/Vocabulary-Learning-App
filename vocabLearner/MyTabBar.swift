03b4677c03b8da39d130e7b30ee59ae8ab602b00
Joseph McGeever <mcgeevej@coventry.ac.uk>
2019-10-07 10:38:38 +0100

    Added 5 tabs, need to link code (see page 373 in book)

---
+++ vocabLearner/MyTabBar.swift
@@ -0,0 +1,30 @@
+//
+//  MyTabBar.swift
+//  vocabLearner
+//
+//  Created by Joseph McGeever on 07/10/2019.
+//  Copyright © 2019 Joseph McGeever. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class MyTabBarController: UITabBarController, UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // tell our UITabBarController subclass to handle its own delegate methods
+        self.delegate = self
+    }
+
+    // called whenever a tab button is tapped
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+
+        if viewController is Home {
+            print("First tab")
+        } else if viewController is Add {
+            print("Second tab")
+        }
+    }
+}
